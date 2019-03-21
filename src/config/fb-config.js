dcec38292087802d34b9e090f5364b0a1c7884f3
EBEREGIT <samsoneberenjoku@gmail.com>
2019-03-21 06:12:27 +0100

    installed and configured firebase in fb-config.js

---
+++ src/config/fb-config.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyB2HKEA2geY722VzrW3fUMj1gOOmnWO76c",
+    authDomain: "ebereplentyapp.firebaseapp.com",
+    databaseURL: "https://ebereplentyapp.firebaseio.com",
+    projectId: "ebereplentyapp",
+    storageBucket: "ebereplentyapp.appspot.com",
+    messagingSenderId: "693020938245"
+  };
+
+  firebase.initializeApp(config);
+  firebase.firestore().settings({ timestampsInSnapshots: true})
+
+  export default firebase;
