2fb571b843160c0ed6981c9a797cca9b94bbb583
NJOKU SAMSON EBERE <samsoneberenjoku@gmail.com>
2019-03-26 10:03:05 +0100

    removed configuration setting for privacy reasons

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -4,12 +4,7 @@ import 'firebase/auth';
 
 // Initialize Firebase
 var config = {
-    apiKey: "AIzaSyB2HKEA2geY722VzrW3fUMj1gOOmnWO76c",
-    authDomain: "ebereplentyapp.firebaseapp.com",
-    databaseURL: "https://ebereplentyapp.firebaseio.com",
-    projectId: "ebereplentyapp",
-    storageBucket: "ebereplentyapp.appspot.com",
-    messagingSenderId: "693020938245"
+    
   };
 
   firebase.initializeApp(config);
